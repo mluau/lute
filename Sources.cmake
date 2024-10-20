324cc18fc3610fb3bdd9d2e72436092fbcc010cd
Aaron Weiss <aweiss@hey.com>
2024-10-19 23:54:30 -0700

    set up cmake

---
+++ Sources.cmake
@@ -0,0 +1,11 @@
+target_sources(Queijo.Lib PRIVATE
+    lib/include/queijo/io.h
+    lib/include/queijo/net.h
+
+    lib/src/io.cpp
+    lib/src/net.cpp
+)
+
+target_sources(Queijo.CLI PRIVATE
+    cli/queijo.cpp
+)
