8dd7a99ab80925f5fad8f98c72ff79b9049dde6e
bjcscat <bjcs.rbx@gmail.com>
2025-03-25 02:16:22 -0500

    Add compilation/loading for bytecode (#120)
    This partially resolves the features I'd like to see out of #85 when it
    comes to loading bytecode but going forward I'd like to see the ability
    to add hooks or control external luau states

--- luau/include/lute/luau.h
+++ luau/include/lute/luau.h
@@ -14,9 +14,15 @@ int luau_parse(lua_State* L);
 
 int luau_parseexpr(lua_State* L);
 
+int compile_luau(lua_State* L);
+
+int load_luau(lua_State* L);
+
 static const luaL_Reg lib[] = {
     {"parse", luau_parse},
     {"parseexpr", luau_parseexpr},
+    {"compile", compile_luau},
+    {"load", load_luau},
     {nullptr, nullptr},
 };
 
--- luau/src/luau.cpp
+++ luau/src/luau.cpp
@@ -6,9 +6,15 @@
 #include "Luau/Parser.h"
 #include "Luau/ParseOptions.h"
 #include "Luau/ToString.h"
+#include "Luau/Compiler.h"
 
 #include "lua.h"
 #include "lualib.h"
+#include <cstddef>
+#include <cstring>
+#include <iterator>
+
+const char* COMPILE_RESULT_TYPE = "CompileResult";
 
 namespace luau
 {
@@ -1319,13 +1325,88 @@ int luau_parseexpr(lua_State* L)
     return 1;
 }
 
+inline int check_int_field(lua_State* L, int obj_idx, const char* field_name, int default_value)
+{
+    if (lua_getfield(L, obj_idx, field_name) == LUA_TNIL)
+        return default_value;
+
+    int is_num;
+    int value = lua_tointegerx(L, -1, &is_num);
+
+    if (!is_num)
+        luaL_errorL(L, "Expected number for field \"%s\"", field_name);
+
+    return value;
+}
+
+int compile_luau(lua_State* L)
+{
+    size_t source_size;
+    const char* source = luaL_checklstring(L, 1, &source_size);
+
+    Luau::CompileOptions opts{};
+
+    if (lua_type(L, 2) == LUA_TTABLE)
+    {
+        opts.optimizationLevel = check_int_field(L, 2, "optimization_level", 1);
+        opts.debugLevel = check_int_field(L, 2, "debug_level", 1);
+        opts.coverageLevel = check_int_field(L, 2, "coverage_level", 1);
+    }
+
+    std::string bytecode = Luau::compile(std::string(source, source_size), opts);
+
+    std::string* userdata = static_cast<std::string*>(lua_newuserdata(L, sizeof(std::string)));
+
+    new (userdata) std::string(std::move(bytecode));
+
+    luaL_getmetatable(L, COMPILE_RESULT_TYPE);
+    lua_setmetatable(L, -2);
+
+    return 1;
+}
+
+int load_luau(lua_State* L)
+{
+    const std::string* bytecode_string = static_cast<std::string*>(luaL_checkudata(L, 1, COMPILE_RESULT_TYPE));
+    const char* chunk_name = luaL_optlstring(L, 2, "luau.load", nullptr);
+
+    luau_load(L, chunk_name, bytecode_string->c_str(), bytecode_string->length(), lua_gettop(L) > 2 ? 3 : 0);
+
+    return 1;
+}
+
 } // namespace luau
+
+static int index_result(lua_State* L) {
+    const std::string* bytecode_string = static_cast<std::string*>(luaL_checkudata(L, 1, COMPILE_RESULT_TYPE));
+
+    if (std::strcmp(luaL_checkstring(L, 2), "bytecode") == 0) {
+        lua_pushlstring(L, bytecode_string->c_str(), bytecode_string->size());
+
+        return 1;
+    }
+    
+    return 0;
+}
+
+// perform type mt registration, etc
+static int init_luau_lib(lua_State* L)
+{
+    luaL_newmetatable(L, COMPILE_RESULT_TYPE);
+
+    lua_pushcfunction(L, index_result, "CompilerResult.__index");
+    lua_setfield(L, -2, "__index");
+
+    lua_pop(L, 1);
+
+    return 1;
+}
 
 int luaopen_luau(lua_State* L)
 {
     luaL_register(L, "luau", luau::lib);
 
-    return 1;
+    return init_luau_lib(L);
 }
 
 int luteopen_luau(lua_State* L)
@@ -1343,5 +1424,5 @@ int luteopen_luau(lua_State* L)
 
     lua_setreadonly(L, -1, 1);
 
-    return 1;
+    return init_luau_lib(L);
 }
