5a8b250e35d2e3a13fe8fbc935d6cccafc87f61c
Nick Winans <nick.win999@gmail.com>
2025-05-02 18:22:04 -0700

    Maintain a ref for the new thread created in net.serve request handling (#252)
    With high amounts of requests it seems like the GC is cleaning in-flight
    threads causing the process to exit. Holding onto a ref is fixing this
    behavior for me.

--- net/src/net.cpp
+++ net/src/net.cpp
@@ -238,6 +238,9 @@ static void processRequest(
 )
 {
     lua_State* L = lua_newthread(state->runtime->GL);
+    luaL_sandboxthread(L);
+    std::shared_ptr<Ref> threadRef = getRefForThread(L);
+    lua_pop(state->runtime->GL, 1);
 
     lua_createtable(L, 0, 5);
 
